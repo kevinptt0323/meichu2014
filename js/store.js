2b1b12f5147abd10c907c4c6cf87bec63568e714
Kevin Lin <kevinptt0323@gmail.com>
2014-12-29 03:14:20 +0800

    add cart page

--- js/store.js
+++ js/store.js
@@ -23,7 +23,7 @@ store.initProducts = function() {
 	});
 },
 store.makeStore = function() {
-	var card = '<div class="card" data-gid="{{gid}}"> <div class="dimmable image"> <div class="ui dimmer"> <div class="content"> <div class="center"> <div class="ui inverted button view"><i class="zoom icon"></i>View</div> </div> </div> </div> <img src="{{src}}"> </div> <div class="content"> <div class="header">{{name}}</div> <div class="meta ui tag label"> <span class="price">{{price}}</span> <span class="special price">{{special}}</span> </div> </div> </div>';
+	var card = '<div class="card" data-gid="{{gid}}"> <div class="dimmable image"> <div class="ui dimmer"> <div class="content"> <div class="center"> <div class="ui inverted button view"><i class="zoom icon"></i>View</div> </div> </div> </div> <img src="{{src}}"> </div> <div class="content"> <a class="header">{{name}}</a> <div class="meta ui tag label price"> <span>{{price}}</span> <span class="special">{{special}}</span> </div> </div> </div>';
 	for(var i=0; i<this.data.length; i++) {
 		var newItem = card.replace("{{name}}", this.data[i].name)
 			.replace("{{gid}}", this.data[i].gid)
@@ -36,7 +36,7 @@ store.makeStore = function() {
 	}
 	$(".dimmable.image").dimmer({ on: "hover" });
 
-	var gwindow = '<i class="close icon"></i> <div class="content"> <div class="ui medium image"> <a class="preview" target="_blank"><img></a> </div> <div class="description"> <div class="ui huge header name"> {{name}} </div> <div class="ui basic segment description"> {{description}} </div> <div class="ui right aligned basic segment"> <div class="meta ui huge tag label"> <span class="price"> {{price}} </span>&nbsp; <span class="special price"> {{special}} </span> </div> </div> </div> </div> <div class="ui divider"></div> <div class="actions"> <div id="selector"></div> <div class="ui medium pagination menu" id="amount"> <a class="minus icon item"> <i class="minus icon"></i> </a> <div class="item"> <div class="ui transparent input"> <input type="text" value="1"> </div> </div> <a class="add icon item"> <i class="plus icon"></i> </a> </div> <div class="ui green button add-to-cart" data-gid="{{gid}}"> <i class="plus icon"></i> Add to Cart </div> </div>';
+	var gwindow = '<i class="close icon"></i> <div class="content"> <div class="ui medium image"> <a class="preview" target="_blank"><img></a> </div> <div class="description"> <div class="ui huge header name"> {{name}} </div> <div class="ui basic segment description"> {{description}} </div> <div class="ui right aligned basic segment"> <div class="meta ui huge tag label price"> <span> {{price}} </span>&nbsp; <span class="special"> {{special}} </span> </div> </div> </div> </div> <div class="ui divider"></div> <div class="actions"> <div id="selector"></div> <div class="ui medium pagination menu" id="amount"> <a class="minus icon item"> <i class="minus icon"></i> </a> <div class="item"> <div class="ui transparent input"> <input type="text" value="1"> </div> </div> <a class="add icon item"> <i class="plus icon"></i> </a> </div> <div class="ui green button add-to-cart" data-gid="{{gid}}"> <i class="plus icon"></i> Add to Cart </div> </div>';
 	$("#goods-window").html(gwindow);
 
 	var obj = this;
@@ -56,8 +56,8 @@ store.view = function(gid) {
 			$gwindow.find(".name.header").html(elem.name);
 			$gwindow.find(".button").attr("data-gid", elem.gid);
 			$gwindow.find("a.preview").attr("href", elem.src).children("img").attr("src", elem.src);
-			$gwindow.find(".price").html(elem.price);
-			$gwindow.find(".special.price").html(elem.special || "");
+			$gwindow.find(".price > span:not(.special)").html(elem.price);
+			$gwindow.find(".price > span.special").html(elem.special || "");
 			$gwindow.find(".description.segment").html(elem.description || "");
 			$gwindow.find("#amount > div > .ui.input input").val(1);
 			if( elem.special )
