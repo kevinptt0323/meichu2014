cbacdc3e7514ad14145e423ec737e7fdd72197e2
Kevin Lin <kevinptt0323@gmail.com>
2014-12-28 23:01:51 +0800

    add dropdown for sub-item

--- js/store.js
+++ js/store.js
@@ -36,7 +36,7 @@ store.makeStore = function() {
 	}
 	$(".dimmable.image").dimmer({ on: "hover" });
 
-	var gwindow = '<i class="close icon"></i> <div class="content"> <div class="ui medium image"> <img class="preview"> </div> <div class="description"> <div class="ui huge header name"> {{name}} </div> <div class="ui basic segment description"> {{description}} </div> <div class="ui right aligned basic segment"> <div class="meta ui huge tag label"> <span class="price"> {{price}} </span>&nbsp; <span class="special price"> {{special}} </span> </div> </div> </div> </div> <div class="ui divider"></div> <div class="actions"> <div id="selector"></div> <div class="ui medium pagination menu"> <a class="icon item"> <i class="minus icon"></i> </a> <div class="item"> <div class="ui transparent input"> <input type="text" value="1"> </div> </div> <a class="icon item"> <i class="plus icon"></i> </a> </div> <div class="ui green basic inverted button add-to-cart" data-gid="{{gid}}"> <i class="plus icon"></i> Add to Cart </div> </div>';
+	var gwindow = '<i class="close icon"></i> <div class="content"> <div class="ui medium image"> <a class="preview" target="_blank"><img></a> </div> <div class="description"> <div class="ui huge header name"> {{name}} </div> <div class="ui basic segment description"> {{description}} </div> <div class="ui right aligned basic segment"> <div class="meta ui huge tag label"> <span class="price"> {{price}} </span>&nbsp; <span class="special price"> {{special}} </span> </div> </div> </div> </div> <div class="ui divider"></div> <div class="actions"> <div id="selector"></div> <div class="ui medium pagination menu"> <a class="minus icon item"> <i class="minus icon"></i> </a> <div class="item"> <div class="ui transparent input"> <input type="text" value="1"> </div> </div> <a class="add icon item"> <i class="plus icon"></i> </a> </div> <div class="ui green basic inverted button add-to-cart" data-gid="{{gid}}"> <i class="plus icon"></i> Add to Cart </div> </div>';
 	$("#goods-window").html(gwindow);
 
 	var obj = this;
@@ -54,7 +54,7 @@ store.view = function(gid) {
 			console.log(elem.name);
 			$gwindow.find(".name.header").html(elem.name);
 			$gwindow.find(".button").attr("data-gid", elem.gid);
-			$gwindow.find("img.preview").attr("src", elem.src);
+			$gwindow.find("a.preview").attr("href", elem.src).children("img").attr("src", elem.src);
 			$gwindow.find(".price").html(elem.price);
 			$gwindow.find(".special.price").html(elem.special || "");
 			$gwindow.find(".description.segment").html(elem.description || "");
@@ -77,12 +77,13 @@ store.view = function(gid) {
 				var $dd = $("#selector > .ui.dropdown");
 				$dd.dropdown({transition:"fade up"});
 			}
-			$gwindow.modal('show')
-				.find(".add-to-cart.button")
-					.unbind('click')
-					.on('click', function(){
-						obj.cart.add(elem.gid, $("#selector > .ui.dropdown").dropdown("get value"));
-					});
+			$gwindow.modal('show');
+			$gwindow.find(".add-to-cart.button")
+				.unbind('click')
+				.on('click', function(){
+					obj.cart.add(elem.gid, $("#selector > .ui.dropdown").dropdown("get value"));
+				});
+			$gwindow.find("")
 		}
 	});
 },
